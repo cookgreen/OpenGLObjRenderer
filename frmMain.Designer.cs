69ef324363fc42d0f184bf57c69a597c337ff758
Cook Green <dell20091234@gmail.com>
2019-11-07 21:31:09 +0800

    Initial Commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ObjRenderer
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.openGLControl1 = new SharpGL.OpenGLControl();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.TextBox();
+            this.txtTexture = new System.Windows.Forms.TextBox();
+            this.btnChooseModel = new System.Windows.Forms.Button();
+            this.btnChooseTexture = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openGLControl1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.openGLControl1, 3);
+            this.openGLControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.openGLControl1.DrawFPS = false;
+            this.openGLControl1.Location = new System.Drawing.Point(3, 63);
+            this.openGLControl1.Name = "openGLControl1";
+            this.openGLControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl1.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl1.Size = new System.Drawing.Size(809, 458);
+            this.openGLControl1.TabIndex = 0;
+            this.openGLControl1.OpenGLInitialized += new System.EventHandler(this.OpenGLControl1_OpenGLInitialized);
+            this.openGLControl1.OpenGLDraw += new SharpGL.RenderEventHandler(this.OpenGLControl1_OpenGLDraw);
+            this.openGLControl1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FrmMain_KeyDown);
+            this.openGLControl1.Resize += new System.EventHandler(this.OpenGLControl1_Resize);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 70F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.tableLayoutPanel1.Controls.Add(this.openGLControl1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtModel, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtTexture, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnChooseModel, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnChooseTexture, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(815, 524);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Model:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Texture:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtModel
+            // 
+            this.txtModel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtModel.Location = new System.Drawing.Point(73, 3);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.ReadOnly = true;
+            this.txtModel.Size = new System.Drawing.Size(674, 21);
+            this.txtModel.TabIndex = 3;
+            // 
+            // txtTexture
+            // 
+            this.txtTexture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtTexture.Location = new System.Drawing.Point(73, 33);
+            this.txtTexture.Name = "txtTexture";
+            this.txtTexture.ReadOnly = true;
+            this.txtTexture.Size = new System.Drawing.Size(674, 21);
+            this.txtTexture.TabIndex = 4;
+            // 
+            // btnChooseModel
+            // 
+            this.btnChooseModel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnChooseModel.Location = new System.Drawing.Point(753, 3);
+            this.btnChooseModel.Name = "btnChooseModel";
+            this.btnChooseModel.Size = new System.Drawing.Size(59, 24);
+            this.btnChooseModel.TabIndex = 5;
+            this.btnChooseModel.Text = "...";
+            this.btnChooseModel.UseVisualStyleBackColor = true;
+            this.btnChooseModel.Click += new System.EventHandler(this.BtnChooseModel_Click);
+            // 
+            // btnChooseTexture
+            // 
+            this.btnChooseTexture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnChooseTexture.Location = new System.Drawing.Point(753, 33);
+            this.btnChooseTexture.Name = "btnChooseTexture";
+            this.btnChooseTexture.Size = new System.Drawing.Size(59, 24);
+            this.btnChooseTexture.TabIndex = 6;
+            this.btnChooseTexture.Text = "...";
+            this.btnChooseTexture.UseVisualStyleBackColor = true;
+            this.btnChooseTexture.Click += new System.EventHandler(this.BtnChooseTexture_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 524);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Render Window";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private SharpGL.OpenGLControl openGLControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtModel;
+        private System.Windows.Forms.TextBox txtTexture;
+        private System.Windows.Forms.Button btnChooseModel;
+        private System.Windows.Forms.Button btnChooseTexture;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.SceneGraph.Assets;
+
+namespace ObjRenderer
+{
+	public partial class frmMain : Form
+	{
+		private Texture texture;
+        private string ObjFile;
+        private string TextureFile;
+		private ObjModel model;
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		double currentX = 0;
+		double currentZ = -55;
+		private float rotation;
+
+        public bool Ready
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(TextureFile) && File.Exists(TextureFile)
+                    && !string.IsNullOrEmpty(ObjFile) && File.Exists(ObjFile);
+            }
+        }
+
+		private void FrmMain_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void OpenGLControl1_OpenGLInitialized(object sender, EventArgs e)
+		{
+			var gl = openGLControl1.OpenGL;
+			gl.ClearColor(0, 0, 0, 0);
+			gl.ShadeModel(OpenGL.GL_SMOOTH);
+			var mat_specular = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+			var mat_ambient = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+			var mat_diffuse = new float[] { 2.0f, 2.0f, 2.0f, 0.1f };
+			var mat_shininess = new float[] { 100.0f };
+			gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SPECULAR, mat_specular);
+			gl.Material(OpenGL.GL_FRONT, OpenGL.GL_AMBIENT, mat_ambient);
+			gl.Material(OpenGL.GL_FRONT, OpenGL.GL_DIFFUSE, mat_diffuse);
+			gl.Material(OpenGL.GL_FRONT, OpenGL.GL_SHININESS, mat_shininess);
+			var ambientLight = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+			var diffuseLight = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+			var posLight0 = new float[] { 2.0f, 0.1f, 0.0f, 0.0f };
+			gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, ambientLight);
+			gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, diffuseLight);
+			gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, posLight0);
+
+			texture = new Texture();
+            if (Ready)
+            {
+                texture.Create(gl, TextureFile);
+            }
+
+			gl.Enable(OpenGL.GL_TEXTURE_2D);
+			gl.Enable(OpenGL.GL_LIGHTING);
+			gl.Enable(OpenGL.GL_LIGHT0);
+			gl.Enable(OpenGL.GL_LIGHT1);
+			gl.Enable(OpenGL.GL_DEPTH_TEST);
+		}
+
+		private void OpenGLControl1_OpenGLDraw(object sender, RenderEventArgs args)
+		{
+			if (Ready)
+			{
+                model = new ObjModel(ObjFile);
+
+                OpenGL gl = this.openGLControl1.OpenGL;
+				gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+				gl.LoadIdentity();
+
+				gl.LookAt(0, 0, currentZ, currentX, 0, 0, 0, 1, 0);
+
+				gl.Rotate(-rotation, 0, 1, 0);
+				rotation += 3.0f;
+
+				for (int i = 0; i < model.Faces.Length; i++)
+				{
+					var face = model.Faces[i];
+					if (face.IsQuad)
+					{
+						gl.Begin(OpenGL.GL_QUADS);
+						{
+							var v1 = model.Vertics[face.Points[0].VertexIndex - 1];
+							var vn1 = model.VertexNormals[face.Points[0].NormalIndex - 1];
+							var vt1 = model.VertexTextures[face.Points[0].TextureIndex - 1];
+
+							var v2 = model.Vertics[face.Points[1].VertexIndex - 1];
+							var vn2 = model.VertexNormals[face.Points[1].NormalIndex - 1];
+							var vt2 = model.VertexTextures[face.Points[1].TextureIndex - 1];
+
+							var v3 = model.Vertics[face.Points[2].VertexIndex - 1];
+							var vn3 = model.VertexNormals[face.Points[2].NormalIndex - 1];
+							var vt3 = model.VertexTextures[face.Points[2].TextureIndex - 1];
+
+							var v4 = model.Vertics[face.Points[3].VertexIndex - 1];
+							var vn4 = model.VertexNormals[face.Points[3].NormalIndex - 1];
+							var vt4 = model.VertexTextures[face.Points[3].TextureIndex - 1];
+
+							gl.TexCoord(vt1.X, vt1.Y * -1);
+							gl.Normal(vn1.X, vn1.Y, vn1.Z);
+							gl.Vertex(v1.X, v1.Y, v1.Z);
+
+							gl.TexCoord(vt2.X, vt2.Y * -1);
+							gl.Normal(vn2.X, vn2.Y, vn2.Z);
+							gl.Vertex(v2.X, v2.Y, v2.Z);
+
+							gl.TexCoord(vt3.X, vt3.Y * -1);
+							gl.Normal(vn3.X, vn3.Y, vn3.Z);
+							gl.Vertex(v3.X, v3.Y, v3.Z);
+
+							gl.TexCoord(vt4.X, vt4.Y * -1);
+							gl.Normal(vn4.X, vn4.Y, vn4.Z);
+							gl.Vertex(v4.X, v4.Y, v4.Z);
+						}
+						gl.End();
+					}
+					else
+					{
+						gl.Begin(SharpGL.Enumerations.BeginMode.Triangles);
+						{
+							var v1 = model.Vertics[face.Points[0].VertexIndex - 1];
+							var vn1 = model.VertexNormals[face.Points[0].NormalIndex - 1];
+							var vt1 = model.VertexTextures[face.Points[0].TextureIndex - 1];
+
+							var v2 = model.Vertics[face.Points[1].VertexIndex - 1];
+							var vn2 = model.VertexNormals[face.Points[1].NormalIndex - 1];
+							var vt2 = model.VertexTextures[face.Points[1].TextureIndex - 1];
+
+							var v3 = model.Vertics[face.Points[2].VertexIndex - 1];
+							var vn3 = model.VertexNormals[face.Points[2].NormalIndex - 1];
+							var vt3 = model.VertexTextures[face.Points[2].TextureIndex - 1];
+
+							gl.TexCoord(vt1.X, vt1.Y * -1);
+							gl.Normal(vn1.X, vn1.Y, vn1.Z);
+							gl.Vertex(v1.X, v1.Y, v1.Z);
+
+							gl.TexCoord(vt2.X, vt2.Y * -1);
+							gl.Normal(vn2.X, vn2.Y, vn2.Z);
+							gl.Vertex(v2.X, v2.Y, v2.Z);
+
+							gl.TexCoord(vt3.X, vt3.Y * -1);
+							gl.Normal(vn3.X, vn3.Y, vn3.Z);
+							gl.Vertex(v3.X, v3.Y, v3.Z);
+						}
+						gl.End();
+
+					}
+					gl.Flush();
+				}
+			}
+		}
+
+		private void OpenGLControl1_Resize(object sender, EventArgs e)
+		{
+			OpenGL gl = openGLControl1.OpenGL;
+
+			gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+			gl.LoadIdentity();
+
+			gl.Perspective(60.0f, (double)Width / (double)Height, 15, 300.0);
+
+			gl.LookAt(-5, 5, -5, 0, 0, 0, 0, -1, 0);
+
+			gl.MatrixMode(OpenGL.GL_MODELVIEW);
+		}
+
+		private void FrmMain_KeyDown(object sender, KeyEventArgs e)
+		{
+			switch(e.KeyCode)
+			{
+				case Keys.S:
+					currentZ -= 5;
+					break;
+				case Keys.W:
+					currentZ += 5;
+					break;
+				case Keys.A:
+					currentX += 5;
+					break;
+				case Keys.D:
+					currentX -= 5;
+					break;
+			}
+		}
+
+        private void BtnChooseModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Obj Model|*.obj";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtModel.Text = dialog.FileName;
+                ObjFile = txtModel.Text;
+            }
+        }
+
+        private void BtnChooseTexture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PNG file|*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txtTexture.Text = dialog.FileName;
+                TextureFile = txtTexture.Text;
+                UpdateTextureImage();
+            }
+        }
+
+        private void UpdateTextureImage()
+        {
+            var gl = openGLControl1.OpenGL;
+            if (texture != null)
+            {
+                texture.Destroy(gl);
+                texture.Create(gl, TextureFile);
+            }
+        }
+    }
+}
